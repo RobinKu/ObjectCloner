fc2fbeb0488b216dd0c4b335b69d87cfd9ec6390
Robin <robin@kuijstermans.net>
2014-04-15 20:26:44 +0200

    Added a cloning action to provide a static value.

---
+++ ObjectCloner/Actions/StaticValueAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObjectCloner.Actions
+{
+    public class StaticValueAction : ICloningAction
+    {
+        private readonly object value;
+
+        public StaticValueAction(object value)
+        {
+            this.value = value;
+        }
+
+        public object Clone(Cloner cloner, string propertyName, object valueFrom)
+        {
+            return this.value;
+        }
+    }
+}
